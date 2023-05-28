a9e12836815e25ec356f7cb85ea7199f5cae5428
usleonid <64957838+usleonid@users.noreply.github.com>
2023-05-28 17:44:50 +0300

    phonebook_upto280523

--- HW_Objects_Phonebook_MyVersion/script.js
+++ HW_Objects_Phonebook_MyVersion/script.js
@@ -1,3 +1,34 @@
+// NODES
+const $contactName = document.getElementById('contactName');
+const $email = document.getElementById('contactEmail');
+const $contactPhone = document.getElementById('contactPhone');
+const $company = document.getElementById('contactCompany');
+const $address = document.getElementById('contactAddress');
+const $contactsList = document.getElementById('contactsList');
+const $addContactBtn = document.getElementById('addContact');
+const $contactForm = document.getElementById('addContactForm');
+const $searchInput = document.getElementById('search-input');
+
+//get values of the contact form
+const getNameFieldValue = () => $contactName.value.trim();
+const getEmailFieldValue = () => $email.value.trim();
+const getPhoneFieldValue = () => $contactPhone.value.trim();
+const getCompanyFieldValue = () => $company.value.trim();
+const getAddressFieldCityValue = () => $address.value.split(",")[0];
+const getAddressFieldStreetValue = () => $address.value.split(",")[1];
+const getAddressFieldHouseValue = () => $address.value.split(",")[2];
+// END OF NODES
+
+
+// EVENT LISTENERS
+// Add contact
+$addContactBtn.addEventListener('click', addHandler);
+// Search
+$searchInput.addEventListener('input', displayContacts);
+// END OF EVENT LISTENERS
+
+
+// MAIN OBJECT "PHONEBOOK"
 const phoneBook = {
     contactsList: [],
     addContact(contact) {
@@ -8,53 +39,36 @@ const phoneBook = {
         const contact = this.contactsList.find(contact => contact.id === contactId);
 
         // Fill the fields of the form to change any of them
-        document.getElementById('contactName').value = contact.contactName;
-        document.getElementById('contactEmail').value = contact.email;
-        document.getElementById('contactPhone').value = contact.phone;
-        document.getElementById('contactCompany').value = contact.company;
+        $contactName.value = contact.contactName;
+        $email.value = contact.email;
+        $contactPhone.value = contact.phone;
+        $company.value = contact.company;
         contact.address.city || contact.address.street || contact.address.house ? 
-            document.getElementById('contactAddress').value = `${phoneBook.contactsList[0].address.city},${phoneBook.contactsList[0].address.street},${phoneBook.contactsList[0].address.house}` : 
-            document.getElementById('contactAddress').value = "";
+            $address.value = `${contact.address.city},${contact.address.street},${contact.address.house}` : 
+            $address.value = "";
     },
     deleteContact(contactID) {
         this.contactsList = this.contactsList.filter(contact => contact.id !== contactID);
     },
 }
+// END OF THE OBJECT "PHONEBOOK"
 
-const $contactsList = document.getElementById('contactsList');
-const $addContactBtn = document.getElementById('addContact');
-const $contactForm = document.getElementById('addContactForm');
-const $searchInput = document.getElementById('search-input');
-
-// EVENT LISTENERS
-
-// Add contact
-$addContactBtn.addEventListener('click', addHandler);
-// Search
-$searchInput.addEventListener('input', displayContacts);
 
 // HANDLERS
-
 // Add contact button handler
 function addHandler(event) {
 
     event.preventDefault();
 
-    const $contactName = document.getElementById('contactName');
-    const $email = document.getElementById('contactEmail');
-    const $phone = document.getElementById('contactPhone');
-    const $company = document.getElementById('contactCompany');
-    const $address = document.getElementById('contactAddress');
-
     const contact = {
-        contactName: $contactName.value.trim(),
-        email: $email.value,
-        phone: $phone.value,
-        company: $company.value.trim(),
+        contactName: getNameFieldValue(),
+        email: getEmailFieldValue(),
+        phone: getPhoneFieldValue(),
+        company: getCompanyFieldValue(),
         address: {
-            city: $address.value.split(",")[0],
-            street: $address.value.split(",")[1] ? $address.value.split(",")[1] : "",
-            house: $address.value.split(",")[2] ? $address.value.split(",")[2] : ""
+            city: getAddressFieldCityValue() ? getAddressFieldCityValue().trim() : "",
+            street: getAddressFieldStreetValue() ? getAddressFieldStreetValue().trim() : "",
+            house: getAddressFieldHouseValue() ? getAddressFieldHouseValue().trim() : ""
         }
     };
     
@@ -114,18 +128,18 @@ function displayContacts() {
         const li = document.createElement('li');
         let textContent = '';
         
-        textContent += contact.contactName ? `Contact name: ${contact.contactName} | ` : '';
-        textContent += contact.email ? `Email: ${contact.email} | ` : '';
-        textContent += contact.phone ? `Phone: ${contact.phone} | ` : '';
-        textContent += contact.company ? `Company: ${contact.company} | ` : '';
+        textContent += contact.contactName ? `<strong>Contact name:</strong> ${contact.contactName} | ` : '';
+        textContent += contact.email ? `<strong>Email:</strong> ${contact.email} | ` : '';
+        textContent += contact.phone ? `<strong>Phone:</strong> ${contact.phone} | ` : '';
+        textContent += contact.company ? `<strong>Company:</strong> ${contact.company} | ` : '';
         if (contact.address.city || contact.address.street || contact.address.house) {
-            const contactAddress = `Address: ${contact.address.city}, ${contact.address.street}, ${contact.address.house}`;
+            const contactAddress = `<strong>Address:</strong> ${contact.address.city}, ${contact.address.street}, ${contact.address.house}`;
             textContent += contactAddress;
         } else {
             textContent += '';
         }
         
-        li.textContent = textContent;
+        li.innerHTML += textContent;
         const spanBtns = document.createElement('span');
         spanBtns.innerHTML = `
             <button id="changeContact" onclick="changeHandler('${contact.id}')">Change details</button>
